bf7b2d3d92b1de1e9993471fbf6ef8becc8bf118
Rustam <valizadarustam@gmail.com>
2021-09-13 20:03:35 +0400

    Initial Commit

---
+++ WildAfrica/WildAfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WildAfricaApp.swift
+//  WildAfrica
+//
+//  Created by Rustam Valizada on 13.09.21.
+//
+
+import SwiftUI
+
+@main
+struct WildAfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
